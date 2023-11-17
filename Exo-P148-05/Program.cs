95ac2b2131611ae0da7e164b207eaed23580b421
Samuel Legrain <samuel.legrain@bstorm.be>
2023-11-17 10:24:01 +0100

    Ajoutez des fichiers projet.

---
+++ Exo-P148-05/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exo_P148_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (double i = 0; i <= 20; i+=0.1)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            for (decimal i = 0; i <= 20; i += 0.1M)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
