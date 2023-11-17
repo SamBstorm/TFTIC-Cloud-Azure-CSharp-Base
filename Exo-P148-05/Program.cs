b6b566a33cd407a4b07680d04da066445d16f809
Samuel Legrain <samuel.legrain@bstorm.be>
2023-11-17 10:55:05 +0100

    Update Exo P148 05

--- Exo-P148-05/Program.cs
+++ Exo-P148-05/Program.cs
@@ -4,10 +4,23 @@
     {
         static void Main(string[] args)
         {
+            //Demande de l'énoncé
+
             //for (double i = 0; i <= 20; i+=0.1)
             //{
             //    Console.WriteLine(i);
             //}
+
+
+            //Solution Quasiment OK (convertion en string avec 2 décimals)
+
+            //for (double i = 0; i <= 20; i += 0.1)
+            //{
+            //    Console.WriteLine(i.ToString("N2"));
+            //}
+
+
+            //Bonne solution
 
             for (decimal i = 0; i <= 20; i += 0.1M)
             {
